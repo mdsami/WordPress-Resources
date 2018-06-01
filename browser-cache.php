98126f8739767d2811a5d30d5582cb3806ecf08b
Ashraful Islam Oli <rock.ashraful@gmail.com>
2018-06-01 20:31:11 +0600

    Create browser-cache.php

---
+++ browser-cache.php
@@ -0,0 +1,16 @@
+> Check Site URL
+
+site_url();
+die();
+
+> CSS & JS Clear Cache
+
+if(site_url() == "http://localhost/wordpress"){
+	define("VERSION", time());
+}else{
+	define("VERSION", wp_get_theme()->get("Version"));
+}
+
+> User VERSION Editing CSS & JS
+
+wp_enqueue_style( "Demo", get_stylesheet_uri(), null, VERSION );
